1b31187b58bc3ea41e563c061d98b4eeaeff737b
vanto-dev <acdevita19@gmail.com>
2023-08-13 04:49:24 -0400

    Added more test data

--- gridiron_gauge_dashboard_assets/src/components/TabsList.tsx
+++ gridiron_gauge_dashboard_assets/src/components/TabsList.tsx
@@ -3,7 +3,10 @@ import {Tab, Tabs, Box } from "@mui/material"
 import TabContext from "@mui/lab/TabContext"
 import TabPanel from "@mui/lab/TabPanel"
 import PlayerGrid from "./PlayerGrid"
-import {test_RBData} from "./test_RBData"
+import {test_RBData} from "../test_data/test_RBData"
+import {test_WRData} from "../test_data/test_WRData"
+import {test_TEData} from "../test_data/test_TEData"
+import {test_QBData} from "../test_data/test_QBData"
 
 export default function TabsList() {
   const [value, setValue] = React.useState("1")
@@ -29,7 +32,13 @@ export default function TabsList() {
           <Tab value="4" label="TE" style={{ color: "#03fcfc" }} />
         </Tabs>
       </Box>
-      <TabPanel value="1">Item One</TabPanel>
+      <TabPanel value="1">
+        <Box sx={{
+          width: "100%", typography: "body1"
+        }}>
+          <PlayerGrid  playerData={test_QBData}/>
+        </Box>
+      </TabPanel>
       <TabPanel value="2">
         <Box sx={{
           width: "100%", typography: "body1"
@@ -37,8 +46,21 @@ export default function TabsList() {
           <PlayerGrid  playerData={test_RBData}/>
         </Box>
       </TabPanel>
-      <TabPanel value="3">Item Three</TabPanel>
-      <TabPanel value="4">Item Four</TabPanel>
+      <TabPanel value="3">
+        <Box sx={{
+          width: "100%", typography: "body1"
+        }}>
+          <PlayerGrid  playerData={test_WRData}/>
+        </Box>
+      </TabPanel>
+
+      <TabPanel value="4">
+        <Box sx={{
+          width: "100%", typography: "body1"
+        }}>
+          <PlayerGrid  playerData={test_TEData}/>
+        </Box>
+      </TabPanel>
     </TabContext>
 
   )
---
+++ gridiron_gauge_dashboard_assets/src/test_data/test_TEData.tsx
@@ -0,0 +1,197 @@
+export const test_TEData = [
+  {
+    playerName: "Rob Gronkowski",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "Josh Jacobs",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "Austin Ekeler",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "Christian McCaffrey",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },{
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+  {
+    playerName: "JiDion",
+    playerPhoto: "https://i.insider.com/6362955db1760d0019d98dda?width=700",
+    playerScore: "9.67",
+  },
+]
